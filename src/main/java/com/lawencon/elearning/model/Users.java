49e5bd989ee835a43c6f39abaaa84877f8bab5df
Muhammad Nur Ali <muhnurali97@gmail.com>
2020-04-15 20:56:36 +0700

    user crud

--- src/main/java/com/lawencon/elearning/model/Users.java
+++ src/main/java/com/lawencon/elearning/model/Users.java
@@ -4,10 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
@@ -19,12 +16,15 @@ public class Users {
 	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
 	@Column(updatable = false, nullable = false)
 	private String userId;
-	@ManyToOne
-	@JoinColumn(name = "category_id")
-	private Categories category;
 	
 	@Column(nullable = false)
 	private String nama, password;
+	
+	@Column(unique = true, nullable = false)
+	private String email;
+	
+	@Column(unique = true, nullable = false)
+	private String username;
 
 	public String getUserId() {
 		return userId;
@@ -32,14 +32,6 @@ public class Users {
 
 	public void setUserId(String userId) {
 		this.userId = userId;
-	}
-
-	public Categories getCategory() {
-		return category;
-	}
-
-	public void setCategory(Categories category) {
-		this.category = category;
 	}
 
 	public String getId() {
@@ -64,6 +56,22 @@ public class Users {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
 	
 	
